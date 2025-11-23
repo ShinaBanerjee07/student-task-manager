68b83911b55f0039f40f33e20f818974aa54e854
Shina Banerjee <shinabanerjee07@gmail.com>
2025-11-23 22:01:29 +0530

    Fix model path case sensitivity for Render deployment

--- backend/routes/tasks.js
+++ backend/routes/tasks.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const Task = require("../models/Task");
+const Task = require("../models/task");
 const auth = require("../middleware/auth");
 
 // Get all tasks of logged-in user
