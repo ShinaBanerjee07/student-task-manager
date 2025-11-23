b531bb2232af47a2b390757bbb2406f60ff7fef9
Shina Banerjee <shinabanerjee07@gmail.com>
2025-11-24 01:13:40 +0530

    Fix CORS and API connection

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://student-task-manager-1.onrender.com/api",   // Render backend URL
+  baseURL: "https://student-task-manager-1.onrender.com/api",
 });
 
 export default API;
